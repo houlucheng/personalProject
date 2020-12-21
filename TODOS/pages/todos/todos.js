43a605264bbc8efd85369dc3812a019bd4ca46c3
houlucheng <luchenghou@caixin.com>
2020-12-21 18:10:42 +0800

    todos

--- TODOS/pages/todos/todos.js
+++ TODOS/pages/todos/todos.js
@@ -1,11 +1,23 @@
 // pages/todos/todos.js
 Page({
   data :{
-    search: "",
+    input: "",
     todos: [
       {name: 'lea Html', completed: false},
       {name: 'lea Css', completed: true},
       {name: 'lea Java', completed: false}
     ],
+  },
+  inputChangeHandl: function(e){
+    this.setData({input:e.detail.value})
+  },
+  addTodoHandle: function(){
+    if(!this.data.input){return}
+    var todos = this.data.todos;
+    todos.push({name: this.data.input,completed: false})
+    this.setData({todos: todos, input: ""})
+  },
+  toggleTodoHandl: function(e){
+    var index = e.currentTarget.dataset.index
   }
 })
