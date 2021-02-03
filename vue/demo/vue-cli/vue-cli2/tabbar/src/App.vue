df777bb806ca2ba30e10d9bcc98c7cf268d29bcf
houlucheng <luchenghou@caixin.com>
2021-02-03 18:13:57 +0800

    tabbar

---
+++ vue/demo/vue-cli/vue-cli2/tabbar/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <div id="tabbar">
+      <div class="tab-bar-item">首页</div>
+      <div class="tab-bar-item">分类</div>
+      <div class="tab-bar-item">购物车</div>
+      <div class="tab-bar-item">我的</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {
+
+  }
+}
+</script>
+
+<style>
+@import "../src/assets/css/base.css";
+
+#tabbar {
+  display: flex;
+}
+
+.tab-bar-item {
+  flex: 1;
+  text-align: center;
+}
+</style>
