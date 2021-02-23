063937c19065c67bc80f775c98736b9636cd8373
houlucheng <luchenghou@caixin.com>
2021-02-23 18:03:41 +0800

    项目首页scroll

--- vue/demo/vue-cli/vue-cli3/supermall/src/components/common/scroll/Scroll.vue
+++ vue/demo/vue-cli/vue-cli3/supermall/src/components/common/scroll/Scroll.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="wrapper" ref="wrapper">
-      <div>
+      <div class="content">
           <slot></slot>
       </div>
   </div>
@@ -11,16 +11,50 @@
 
 export default {
   name: 'Scroll',
+  props: {
+    probeType: {
+      type: Number,
+      default: 0
+    },
+    pullUpLoad: {
+      type: Boolean,
+      default: false
+    }
+  },
   data() {
     return {
         scroll: null
     }
   },
   mounted() {
-    this.scroll = new BScroll(this.$refs.wrapper, {})
-    setTimeout(()=>{
-        this.scroll.refresh()
+    this.scroll = new BScroll(this.$refs.wrapper, {
+      click: true,
+      probeType: this.probeType,
+      pullUpLoad: this.pullUpLoad
     })
+    // 监听滚动位置
+    this.scroll.on("scroll", (position) => {
+      // console.log(position);
+      this.$emit('scroll', position)
+    })
+    // 上拉加载
+    this.scroll.on("pullingUp", () => {
+      this.$emit("pullingUp")
+    })
+    // window.addEventListener('load', () => {
+    //   this.scroll.refresh()
+    // })
+  },
+  methods: {
+    scrollTo(x, y, time=300) {
+      this.scroll.scrollTo(x, y, time)
+    },
+    finishPullUp() {
+      this.scroll.finishPullUp();
+    },
+    refresh() {
+      this.scroll.refresh()
+    }
   },
 
 }
--- vue/demo/vue-cli/vue-cli3/supermall/src/views/home/Home.vue
+++ vue/demo/vue-cli/vue-cli3/supermall/src/views/home/Home.vue
@@ -2,70 +2,17 @@
   <div id="home">
     <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
     
-    <scroll class="content" ref="a">
+    <scroll class="content" ref="scroll" :probe-type="3" @scroll="contentScroll" :pull-up-load="true" @pullingUp="loadMore">
       <home-swiper :banners="banners" />
       <recommend-view :recommends="recommends" />
       <feature-view></feature-view>
       <tab-control class="tab-control" :titles="['流行', '新款', '精选']" @tabClick="tabClick" />
       <goods-list :goods="showGoods" />
     </scroll>
-
-    <ul>
-      <li>hello1</li>
-      <li>hello2</li>
-      <li>hello3</li>
-      <li>hello4</li>
-      <li>hello5</li>
-      <li>hello6</li>
-      <li>hello7</li>
-      <li>hello8</li>
-      <li>hello9</li>
-      <li>hello10</li>
-      <li>hello11</li>
-      <li>hello12</li>
-      <li>hello13</li>
-      <li>hello14</li>
-      <li>hello15</li>
-      <li>hello16</li>
-      <li>hello17</li>
-      <li>hello18</li>
-      <li>hello19</li>
-      <li>hello20</li>
-      <li>hello21</li>
-      <li>hello22</li>
-      <li>hello23</li>
-      <li>hello24</li>
-      <li>hello25</li>
-      <li>hello26</li>
-      <li>hello27</li>
-      <li>hello28</li>
-      <li>hello29</li>
-      <li>hello30</li>
-      <li>hello31</li>
-      <li>hello32</li>
-      <li>hello33</li>
-      <li>hello34</li>
-      <li>hello35</li>
-      <li>hello36</li>
-      <li>hello37</li>
-      <li>hello38</li>
-      <li>hello39</li>
-      <li>hello40</li>
-      <li>hello41</li>
-      <li>hello42</li>
-      <li>hello43</li>
-      <li>hello44</li>
-      <li>hello45</li>
-      <li>hello46</li>
-      <li>hello47</li>
-      <li>hello48</li>
-      <li>hello49</li>
-      <li>hello50</li>
-    </ul>
+    <back-top @click.native="backClick" v-show="isShowBackTop" />
   </div>
 </template>
 <script>
-import Scroll from "components/common/scroll/Scroll"
 
 import HomeSwiper from "./childComps/HomeSwiper";
 import RecommendView from "./childComps/RecommendView";
@@ -74,6 +21,8 @@ import FeatureView from "./childComps/FeatureView";
 import NavBar from "components/common/navbar/NavBar";
 import TabControl from "components/content/tabControl/TabControl"
 import GoodsList from "components/content/goods/GoodsList"
+import Scroll from "components/common/scroll/Scroll"
+import BackTop from "components/content/backTop/BackTop"
 
 import {getHomeMultidata, getHomeGoods} from "network/home";
 
@@ -88,7 +37,8 @@ export default {
         new: {page: 0, list: []},
         sell: {page: 0, list: []}
       },
-      currentType: "pop"
+      currentType: "pop",
+      isShowBackTop: false
     }
   },
   created() {
@@ -104,6 +54,15 @@ export default {
     
   },
   methods: {
+    loadMore() {
+      this.getHomeGoods(this.currentType)
+    },
+    contentScroll(position) {
+      this.isShowBackTop = (-position.y) > 1000
+    },
+    backClick() {
+      this.$refs.scroll.scrollTo(0,0)
+    },
     tabClick(index) {
       switch(index) {
         case 0:
@@ -128,6 +87,9 @@ export default {
       getHomeGoods(type,page).then(res => {
         this.goods[type].list.push(...res.data.list)
         this.goods[type].page += 1
+
+        this.$refs.scroll.finishPullUp();
+        this.$refs.scroll.refresh();
       })
     }
   },
@@ -138,13 +100,16 @@ export default {
     NavBar,
     TabControl,
     GoodsList,
-    Scroll
+    Scroll,
+    BackTop
   }
 }
 </script>
 <style scoped>
   #home {
     padding-top: 44px;
+    height: 100vh;
+    position: relative;
   }
   .home-nav {
     background-color: var(--color-tint);
@@ -161,7 +126,16 @@ export default {
     z-index: 9;
   }
   .content {
-    height: 300px;
     overflow: hidden;
+    position: absolute;
+    top: 44px;
+    bottom: 49px;
+    left: 0;
+    right: 0;
   }
+  /* .content {
+    height: calc(100% - 93px);
+    overflow: hidden;
+    margin-top: 44px;
+  } */
 </style>
