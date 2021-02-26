8c76b3244b1526865ad7fe21729c4526f6c7592c
houlucheng <luchenghou@caixin.com>
2021-02-26 18:12:31 +0800

    详情页

--- vue/demo/vue-cli/vue-cli3/supermall/src/views/detail/Detail.vue
+++ vue/demo/vue-cli/vue-cli3/supermall/src/views/detail/Detail.vue
@@ -4,7 +4,9 @@
     <scroll class="content" ref="scroll" :probe-type="3">
       <detail-swiper :top-images="topImages" />
       <detail-base-info :goods="goods" />
-      <detail-shop-info :shop="shop"></detail-shop-info>
+      <detail-shop-info :shop="shop" />
+      <detail-goods-info :detail-info="detailInfo" />
+      <detail-param-info :paramInfo="paramInfo" />
     </scroll>
   </div>
 </template>
@@ -13,10 +15,13 @@ import DetailNavBar from "./childComps/DetailNavBar"
 import DetailSwiper from "./childComps/DetailSwiper"
 import DetailBaseInfo from './childComps/DetailBaseInfo'
 import DetailShopInfo from './childComps/DetailShopInfo'
+import DetailGoodsInfo from './childComps/DetailGoodsInfo'
+import DetailParamInfo from './childComps/DetailParamInfo'
 
 import Scroll from 'components/common/scroll/Scroll'
+import {debounce} from "common/utils"
 
-import {getDetail, Goods, Shop} from "network/detail"
+import {getDetail, Goods, Shop, GoodsParam} from "network/detail"
 
 export default {
   name: 'Detail',
@@ -25,12 +30,20 @@ export default {
       iid: null,
       topImages: [],
       goods: {},
-      shop: {}
+      shop: {},
+      detailInfo: {},
+      paramInfo: {},
     }
   },
   created() {
     this.iid = this.$route.params.iid
     this.getDetail()
+  },
+  mounted() {
+    const refresh = debounce(this.$refs.scroll.refresh, 300)
+    this.$bus.$on('imageLoad', () => {
+      refresh()
+    })
   },
   methods: {
     getDetail() {
@@ -40,6 +53,8 @@ export default {
         this.topImages = data.itemInfo.topImages;
         this.goods = new Goods(data.itemInfo, data.columns, data.shopInfo.services)
         this.shop = new Shop(data.shopInfo)
+        this.detailInfo = data.detailInfo
+        this.paramInfo = new GoodsParam(data.itemParams.info, data.itemParams.rule)
       })
     }
   },
@@ -48,7 +63,9 @@ export default {
     DetailSwiper,
     DetailBaseInfo,
     DetailShopInfo,
-    Scroll
+    Scroll,
+    DetailGoodsInfo,
+    DetailParamInfo
   }
 }
 </script>
@@ -62,7 +79,6 @@ export default {
   }
   .detail-nav {
     position: relative;
-    top: 0;
     background-color: #fff;
   }
   .content {
--- vue/demo/vue-cli/vue-cli3/supermall/src/views/detail/childComps/DetailSwiper.vue
+++ vue/demo/vue-cli/vue-cli3/supermall/src/views/detail/childComps/DetailSwiper.vue
@@ -1,7 +1,7 @@
 <template>
     <swiper class="detail-swiper">
       <swiper-item v-for="(item,index) in topImages" :key="index">
-        <img :src="item" alt="">
+        <img :src="item" alt="" @load="imageLoad">
       </swiper-item>
     </swiper>
 </template>
@@ -16,6 +16,11 @@ export default {
       default() {
         return []
       }
+    }
+  },
+  methods: {
+    imageLoad() {
+      this.$bus.$emit("imageLoad")
     }
   },
   components: {
