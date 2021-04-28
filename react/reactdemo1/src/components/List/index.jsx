12ffd118126021957f67dc2813222c1d3eec76ea
houlucheng <32925687+houlucheng@users.noreply.github.com>
2021-04-28 21:29:11 +0800

    list

--- react/reactdemo1/src/components/List/index.jsx
+++ react/reactdemo1/src/components/List/index.jsx
@@ -1,15 +1,21 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import Item from '../Item'
 import './index.css'
 
 export default class List extends Component {
+  static propTypes = {
+    todos: PropTypes.array.isRequired,
+    updateTodo: PropTypes.func.isRequired,
+    deleteTodo: PropTypes.func.isRequired
+  }
   render() {
-    let {todos} = this.props
+    let {todos, updateTodo, deleteTodo} = this.props
     return (
       <ul className="todo-main">
         {
           todos.map( item => {
-            return <Item key={item.id} {...item}/>
+            return <Item updateTodo={updateTodo} deleteTodo={deleteTodo} key={item.id} {...item}/>
           })
         }
       </ul>
