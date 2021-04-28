c863e917dc8545c25fd1df9a94bdf45f7167afb0
houlucheng <32925687+houlucheng@users.noreply.github.com>
2021-04-28 21:28:27 +0800

    item

--- react/reactdemo1/src/components/Item/index.jsx
+++ react/reactdemo1/src/components/Item/index.jsx
@@ -2,15 +2,37 @@ import React, { Component } from 'react'
 import './index.css'
 
 export default class Item extends Component {
+  state = {mouseState: false}
+
+  mouseHandel = (mouseType)=> {
+    return ()=> {
+      this.setState({mouseState: mouseType})
+    }
+  }
+
+  checkedHandel = (id)=>{
+    return (event)=> {
+      const {updateTodo} = this.props
+      updateTodo(id, event.target.checked)
+    }
+  }
+  handelDelete = (id)=> {
+    const {deleteTodo} = this.props
+    if(window.confirm('确定删除吗？')){
+      deleteTodo(id)
+    }
+  }
+
   render() {
-    let {name, done} = this.props
+    let {id, name, done} = this.props
+    let {mouseState} = this.state
     return (
-      <li>
+      <li style={{background: mouseState? '#ddd' : '#fff'}} onMouseEnter={this.mouseHandel(true)} onMouseLeave={this.mouseHandel(false)}>
         <label>
-          <input type="checkbox" defaultChecked={done}/>
+          <input type="checkbox" checked={done} onChange={this.checkedHandel(id)}/>
           <span>{name}</span>
         </label>
-        <button className="btn btn-danger" style={{ display: 'none' }}>删除</button>
+        <button onClick={()=>this.handelDelete(id)} className="btn btn-danger" style={{ display: mouseState ? 'block' : 'none' }}>删除</button>
       </li>
     )
   }
