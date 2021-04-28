53970a26738f6b80b12c19d8b6f3e55742e1a2c8
houlucheng <32925687+houlucheng@users.noreply.github.com>
2021-04-28 21:26:59 +0800

    footer

--- react/reactdemo1/src/components/Footer/index.jsx
+++ react/reactdemo1/src/components/Footer/index.jsx
@@ -1,17 +1,32 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import './index.css'
 
 export default class Footer extends Component {
+  state = {
+    count: 0
+  }
+  static propTyps = {
+    todos: PropTypes.array.isRequired
+  }
+  handelCheckedAll = (event)=> {
+    this.props.checkAllTodo(event.target.checked)
+  }
+  handelClearAll= ()=> {
+    this.props.clearAllDone()
+  }
   render() {
+    const {todos} = this.props
+    let doneTodos = todos.filter(item=> item.done === true)
     return (
       <div className="todo-footer">
         <label>
-          <input type="checkbox" />
+          <input type="checkbox" checked={todos.length === doneTodos.length && todos.length !== 0?true:false} onChange={this.handelCheckedAll} />
         </label>
         <span>
-          <span>已完成0</span> / 全部0
+          <span>已完成{doneTodos.length}</span> / 全部{todos.length}
         </span>
-        <button className="btn btn-danger">清除已完成任务</button>
+        <button className="btn btn-danger" onClick={this.handelClearAll}>清除已完成任务</button>
       </div>
     )
   }
