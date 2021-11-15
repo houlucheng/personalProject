f40da94c850f255c6b5ebab79ca0724238db2c0c
DESKTOP-54I4QNA\admin <1986222509@qq.com>
2021-11-15 18:35:16 +0800

    修改

--- react/stu-shop-manager/src/pages/Login.js
+++ react/stu-shop-manager/src/pages/Login.js
@@ -1,9 +1,61 @@
 import React from 'react'
+// import { Cascader } from 'antd';
+import options from './region'
+import { Cascader } from 'rootnet-design'
+
 
 export default function Login() {
+
+  let obj = [
+    {
+      label: "季报",
+      value: "2",
+      children: [
+        {
+          label: 2023,
+          value: '2',
+          children: [
+            {value: "1", label: "Q1"},  
+            {value: "2", label: "Q2"},
+            {value: "3", label: "Q3"},
+            {value: "4", label: "Q4"}
+          ]
+        },
+        {
+          label: 2022,
+          value: 2022,
+          children: [
+            {value: "1", label: "Q1"},
+            {value: "2", label: "Q2"},
+            {value: "3", label: "Q3"},
+            {value: "4", label: "Q4"}
+          ]
+        },
+        {
+          label: 2021,
+          value: 2021,
+          children: [
+            {value: "1", label: "Q1"},
+            {value: "2", label: "Q2"},
+            {value: "3", label: "Q3"},
+            {value: "4", label: "Q4"}
+          ]
+        }
+      ]
+    }
+   
+  ]
+  
+  function onChange(value,label) {
+    console.log(value, label);
+  }
+
+
+
   return (
     <div>
       <h1>登录</h1>
+      <Cascader options={obj} close onChange={onChange} placeholder="Please select" />
     </div>
   )
 }
